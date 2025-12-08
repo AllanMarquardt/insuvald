ba05bd0a2a45b7c7562c145702d7d0230602920f
AllanMarquardt <abmarquardts@gmail.com>
2025-12-08 16:14:47 -0300

    Socials clickeables agregados

---
+++ src/components/shared/SocialContact.jsx
@@ -0,0 +1,15 @@
+import WhatsApp from '../../assets/images/whatsapp-logo.webp';
+import Instagram from '../../assets/images/instagram-logo.webp';
+
+export default function SocialContact(){
+    return (
+        <div className='flex gap-3 fixed bottom-5 right-5 sm:bottom-12 sm:right-12 z-50'>
+            <a href="https://wa.me/56982638704" target="_blank" rel="noopener noreferrer">
+                <img src={WhatsApp} alt="WhatsApp Logo" className='h-12 w-12 sm:w-18 sm:h-18 hover:scale-110 transition duration-300 opacity-60 hover:opacity-100'/>
+            </a>
+            <a href="https://www.instagram.com/insuvald" target="_blank" rel="noopener noreferrer">
+                <img src={Instagram} alt="Instagram Logo" className='h-12 w-12 sm:w-18 sm:h-18 hover:scale-110 transition duration-300 opacity-60 hover:opacity-100'/>
+            </a>
+        </div>
+    )
+}
